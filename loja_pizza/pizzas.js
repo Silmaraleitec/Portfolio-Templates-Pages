5d24a226bc86932319c4ced4af03fd7e67166efa
Silmara Leite Correia <silmara.leitec@gmail.com>
2021-04-14 16:12:41 -0300

    Add files via upload

---
+++ loja_pizza/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    { id: 1, name: 'Calabresa', img: 'images/pizza.png', price: 20.19, sizes: ['100g', '530g', '860g'], description: 'Uma pizza simples, mas que conquistou uma legião de fãs em todo o mundo. Está presente em mais de 30% dos pedidos.' },
+    { id: 2, name: 'Portuguesa', img: 'images/pizza2.png', price: 18.00, sizes: ['320g', '530g', '860g'], description: 'Ovos, cebola, azeitona, ervilha, queijo e presunto dão sabor a segunda pizza mais queridinha dos brasileiros.' },
+    { id: 3, name: 'Marguerita', img: 'images/pizza3.png', price: 17.45, sizes: ['320g', '530g', '860g'], description: 'Sabor ícone da Itália que leva apenas molho, muçarela, tomate e manjericão. É uma ótima opção para quem não come carne.' },
+    { id: 4, name: 'Frango com catupiry', img: 'images/pizza4.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'A mistura de proteína e cremosidade que deu muito certo.' },
+    { id: 5, name: 'Napolitana', img: 'images/pizza5.png', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Um sabor genuinamente italiano que não pode faltar na sua pizzaria.' },
+    { id: 6, name: 'Romeu e Julieta', img: 'images/pizza6.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: 'A combinação mineira de queijo branco e goiabada faz sucesso em todo o país. Diferentes versões recebem queijos suaves, como brie, ricota e muçarela de búfala.' },
+    { id: 7, name: 'Brigadeiro', img: 'images/pizza7.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Simples e perfeita como tem que ser. A número 1 entre os paulistanos.' }
+];
